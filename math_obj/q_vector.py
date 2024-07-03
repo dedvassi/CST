339937f7bf440cf9763ad23cd3d87eefcdf16266
dedvassi <dedvassi@gmail.com>
2024-07-03 19:08:32 +0500

    Дополнена логика определения вектора в разных СК. В экваториальную инерциальную геоцентрическую СК можно перейти, если известно состояние вектора в любой другой. Переход в Стартовую инерциальную и Гринвичскую проще всего завязать на изначальном переходе в экваториальную, а из нее в них. Если переход в экваториальную не удастся, это означает, что допущены следующие гипотетические ошибки: - Вектор в любой другой СК определен некорректно (например, не является массивом); - Вектор вообще не определился по каким-либо причинам; - Вероятно есть дыры в логике со стороны класса. Таковые будут устраняться в ходе выявления

--- math_obj/q_vector.py
+++ math_obj/q_vector.py
@@ -11,6 +11,18 @@ class Q_v_state():
         self.matrix = matrix
         self.init_data = start_param
 
+        self.__q_st_in = None
+        self.__q_ekv_in = None
+        self.__q_gr = None
+
+        self.__r_st_in = None
+        self.__r_ekv_in = None
+        self.__r_gr = None
+
+        self.__v_st_in = None
+        self.__v_ekv_in = None
+        self.__v_gr = None
+
         if self.init_data[7] == 1:
             print('Стартовая инерциальная геоцентрическая СК')
             self.__q_st_in = self.load_Q()
@@ -32,12 +44,33 @@ class Q_v_state():
 
         if self.init_data[7] == 2:
             print('Экваториальная инерциальная геоцентрическая СК')
-            self.q_st = self.load_data_q_st()
+            self.__q_ekv_in = self.load_Q()
+            ksi_1, ksi_2, ksi_3, v_ksi_1, v_ksi_2, v_ksi_3 = self.__q_ekv_in[:6]
+            self.__r_ekv_in = np.array([ksi_1, ksi_2, ksi_3])
+            self.__v_ekv_in = np.array([v_ksi_1, v_ksi_2, v_ksi_3])
+
+            self.__q_st_in = None
+            self.__r_st_in = None
+            self.__v_st_in = None
+
+            self.__q_gr = None
+            self.__r_gr = None
+            self.__v_gr = None
 
         if self.init_data[7] == 3:
             print('Гринвичская СК')
-            self.q_st = self.load_data_q_st()
-        
+            self.__q_gr = self.load_Q()
+            ksi_gr_1, ksi_gr_2, ksi_gr_3, v_ksi_gr_1, v_ksi_gr_2, v_ksi_gr_3 = self.__q_ekv_in[:6]
+            self.__r_gr = np.array([ksi_gr_1, ksi_gr_2, ksi_gr_3])
+            self.__v_gr = np.array([v_ksi_gr_1, v_ksi_gr_2, v_ksi_gr_3])
+
+            self.__q_st_in = None
+            self.__r_st_in = None
+            self.__v_st_in = None
+
+            self.__q_ekv_in = None
+            self.__r_ekv_in = None
+            self.__v_ekv_in = None
 
     def load_Q(self):
         """
@@ -63,7 +96,13 @@ class Q_v_state():
     @property
     def q_st_in(self):
         if self.__q_st_in is None:
-            pass
+            if self.__q_ekv_in is None:
+                self.__q_ekv_in = self.q_ekv_in
+
+            self.__r_st_in = np.dot(self.matrix.d, self.__r_ekv_in)
+            self.__v_st_in = np.dot(self.matrix.d, self.__v_ekv_in)
+            self.__q_st_in = np.concatenate((self.__r_st_in, self.__v_st_in, np.array([self.__q_ekv_in[6]])))
+
         return self.__q_st_in
 
     @property
@@ -78,21 +117,24 @@ class Q_v_state():
 
             elif self.__q_gr is not None:
                 print('расчет прошел через q_gr')
-                self.__r_gr = np.dot(self.matrix.d.T, self.__r_st_in)
-                self.__v_gr = np.dot(self.matrix.d.T, self.__v_st_in)
-                self.__q_ekv_in = np.concatenate((self.__r_ekv_in, self.__v_ekv_in, np.array([self.__q_st_in[6]])))
-
+                self.__r_ekv_in = np.dot(self.matrix.T_G.T, self.__r_gr)
+                self.__v_ekv_in = np.dot(self.matrix.T_G.T, self.__v_gr) + np.cross(self.matrix.Ω_vector, self.__r_ekv_in)
+                self.__q_ekv_in = np.concatenate((self.__r_ekv_in, self.__v_ekv_in, np.array([self.__q_gr[6]])))
+            else:
+                raise ValueError("Произошла ошибка в свойстве 'q_ekv_in'\n"
+                                 "Не найдено иных представлений вектора")
 
         return self.__q_ekv_in
 
-    def q_gr_vector(self):
-        pass
+    @property
+    def q_gr(self):
+        if self.__q_gr is None:
+            if self.__q_ekv_in is None:
+                self.__q_ekv_in = self.q_ekv_in
 
-#a = Q_v_state()
-#print(a.__dict__)
+            self.__r_gr = np.dot(self.matrix.T_G, self.__r_ekv_in)
+            self.__v_gr = np.dot(self.matrix.T_G, self.__v_ekv_in) - np.cross(self.matrix.Ω_vector, self.__r_gr)
+            self.__q_gr = np.concatenate((self.__r_gr, self.__v_gr, np.array([self.__q_ekv_in[6]])))
+        return self.__q_gr
 
-#a=np.array([1,2,3,4])
-#print(type(a)
-
-#print(isinstance(a, array))
 
