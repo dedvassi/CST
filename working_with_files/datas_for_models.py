3e6fe4bd768f9d8b236481d9e8e14dcc796524aa
dedvassi <144363426+dedvassi@users.noreply.github.com>
2024-07-24 17:15:53 +0500

    Добавлен функционал в datas_for_models.py

--- working_with_files/datas_for_models.py
+++ working_with_files/datas_for_models.py
@@ -1,5 +1,5 @@
 import pandas as pd
-def check_csv_file_atmo(model, month):
+def check_csv_file_atmo(model):
 
     if model == 0:
         pass
@@ -16,6 +16,81 @@ def check_csv_file_atmo(model, month):
 
         cache = {h: (ro, T) for h, ro, T in zip(h_values, ro_values, T_values)}
 
+        return cache
+
+    if model == 2:
+        # Пути к файлам CSV
+        csv_file_path_st_atm = '../data/immutable/stand_atmo_data.csv'
+        csv_file_path_tplot = '../data/immutable/TPLOT.csv'
+        csv_file_path_tdavl = '../data/immutable/TDAVL.csv'
+        csv_file_path_ttemp = '../data/immutable/TTEMP.csv'
+        csv_file_path_tvetz = '../data/immutable/TVETZ.csv'
+        csv_file_path_tvetm = '../data/immutable/TVETM.csv'
+
+        # Загрузка CSV файлов в DataFrame
+        df_st_atm = pd.read_csv(csv_file_path_st_atm)
+        df_tplot = pd.read_csv(csv_file_path_tplot)
+        df_tdavl = pd.read_csv(csv_file_path_tdavl)
+        df_ttemp = pd.read_csv(csv_file_path_ttemp)
+        df_tvetz = pd.read_csv(csv_file_path_tvetz)
+        df_tvetm = pd.read_csv(csv_file_path_tvetm)
+
+        h_values = df_st_atm['h [м]'].values
+        ro_values = df_st_atm['ro [кг/м3]'].values
+        T_values = df_st_atm['T [градус K]'].values
+
+        cache_st_atm = {h: (ro, T) for h, ro, T in zip(h_values, ro_values, T_values)}
+
+        cr_values = df_tplot['Cr'].values
+        k_values = df_tplot['k'].values
+        l_values = df_tplot['l'].values
+        m_values = df_tplot['m'].values
+        n_values = df_tplot['n'].values
+
+        cache_tplot = {cr: (k, l, m, n) for cr, k, l, m, n in zip(cr_values, k_values, l_values, m_values, n_values)}
+
+        cp_values = df_tdavl['Cp'].values
+        k_values = df_tdavl['k'].values
+        l_values = df_tdavl['l'].values
+        m_values = df_tdavl['m'].values
+        n_values = df_tdavl['n'].values
+
+        cache_tdavl = {cp: (k, l, m, n) for cp, k, l, m, n in zip(cp_values, k_values, l_values, m_values, n_values)}
+
+        ct_values = df_ttemp['Ct'].values
+        k_values = df_ttemp['k'].values
+        l_values = df_ttemp['l'].values
+        m_values = df_ttemp['m'].values
+        n_values = df_ttemp['n'].values
+
+        cache_ttemp = {ct: (k, l, m, n) for ct, k, l, m, n in zip(ct_values, k_values, l_values, m_values, n_values)}
+
+        cwz_values = df_tvetz['Cwz'].values
+        k_values = df_tvetz['k'].values
+        l_values = df_tvetz['l'].values
+        m_values = df_tvetz['m'].values
+        n_values = df_tvetz['n'].values
+
+        cache_vetz = {cwz: (k, l, m, n) for cwz, k, l, m, n in zip(cwz_values, k_values, l_values, m_values, n_values)}
+
+        cwm_values = df_tvetm['Cwm'].values
+        k_values = df_tvetm['k'].values
+        l_values = df_tvetm['l'].values
+        m_values = df_tvetm['m'].values
+        n_values = df_tvetm['n'].values
+
+        cache_vetm = {cwm: (k, l, m, n) for cwm, k, l, m, n in zip(cwm_values, k_values, l_values, m_values, n_values)}
+
+        cache = {
+            'stand_atmo': cache_st_atm,
+            'tplot': cache_tplot,
+            'tdavl': cache_tdavl,
+            'ttemp': cache_ttemp,
+            'tvetz': cache_vetz,
+            'tvetm': cache_vetm,
+        }
+
+        print(cache)
         return cache
 
 def check_csv_file_TCxM(priority):
