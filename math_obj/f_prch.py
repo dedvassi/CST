9b6237018bddef0380ef2da48b8799c4923ab7b7
dedvassi <144363426+dedvassi@users.noreply.github.com>
2024-07-30 02:43:54 +0500

    изменения в f_prch.py
    Добавлена логика мультипроцессинга для обработки таблиц глобальной атмосферы, а также шаблон для их анализа

--- math_obj/f_prch.py
+++ math_obj/f_prch.py
@@ -1,7 +1,47 @@
 import numpy as np
+import multiprocessing
 from working_with_files.datas_for_models import *
 
-class F_prch_maker():
+
+def delta_param_worker(input_queue, output_queue, atmo_cache, month):
+    while True:
+        key, B, L, H = input_queue.get()
+        if key is None:
+            break
+        delta = delta_param(atmo_cache, month, key, B, L, H)
+        output_queue.put((key, delta))
+        input_queue.task_done()
+
+# Глобальная функция
+def delta_param(atmo_cache: dict, month: int, key: str, B: float, L: float, H: float):
+    delta = 0
+    for i in (atmo_cache[key]).keys():
+        c, k, l, m, n = (atmo_cache[key])[i]
+
+        Fk = np.cos(np.pi * H * (k - 1) / 102)
+
+        if l == 1:
+            Sl = 1
+        elif l % 2 == 0:
+            Sl = np.cos(np.pi * l * (month - 1) / 12)
+        elif l % 2 == 1:
+            Sl = np.sin(np.pi * (l - 1) * (month - 1) / 12)
+
+        Gm = np.cos((B + np.pi / 2) * (m - 1))
+
+        if n == 1:
+            Rn = 1
+        elif n % 2 == 0:
+            Rn = np.cos(L * n / 2)
+        elif n % 2 == 1:
+            Rn = np.sin(L * (n - 1) / 2)
+
+        delta += c * Fk * Sl * Gm * Rn
+
+    return delta
+
+
+class F_prch_maker:
     """Собирает функцию правой части в зависимости от установленных моделей"""
 
     def __init__(self,
@@ -15,10 +55,10 @@ class F_prch_maker():
                  R_g=287.05287,
                  s_mid_otd=13.2,
                  m_otd=4000):
-        super().__init__()
         self.__start_param = np.concatenate((np.array([grav_m]), np.array([atmo_m])))
         self.__Cx_priority = Cx_priority
         self.__omega_z = omega_z
+        self.__month = month
         self.__fM = fM
         self.__alpha_cs = alpha_cs
         self.__R_g = R_g
@@ -41,6 +81,20 @@ class F_prch_maker():
         self.__r_comp = self.__r_component()
         self.__grav_accel = self.__gravitational_acceleration()
         self.__aero_d_resist = self.__aerodynamics_resist()
+
+        if self.__atmo_model == 2:
+            keys = ['tplot', 'tdavl', 'ttemp', 'tvetz', 'tvetm']
+            self.__process = {}
+
+            for key in keys:
+                input_queue = multiprocessing.JoinableQueue()
+                output_queue = multiprocessing.Queue()
+                process = multiprocessing.Process(target=delta_param_worker, args=(input_queue,
+                                                                                           output_queue,
+                                                                                           self.__atmo_cache,
+                                                                                           self.__month), name=f'proc-{key}')
+                self.__process[key] = (process, input_queue, output_queue)
+                process.start()
 
     def __call__(self):
         return self.f_prch
@@ -103,7 +157,8 @@ class F_prch_maker():
 
         if self.__atmo_model == 1:
             # Стандартная модель атмосферы
-            def analysis_atmo(h):
+            def analysis_atmo(q):
+                h = q.height
                 h_values = sorted(self.__atmo_cache.keys())
                 h_min = h_values[0]
                 h_max = h_values[-1]
@@ -111,18 +166,18 @@ class F_prch_maker():
                 if h <= h_min:
                     # Высота меньше минимальной из таблицы или совпадает с ней
                     h = h_min
-                    ro, T = self.__atmo_cache[h]
+                    ro, T, _, _, _ = self.__atmo_cache[h]
                     return ro, T
                 if h >= h_max:
                     # Высота превышает максимальную из таблицы или совпадает с ней
                     h = h_max
-                    ro, T = self.__atmo_cache[h]
+                    ro, T, _, _, _ = self.__atmo_cache[h]
                     return ro, T
                 else:
                     # Высота находится в пределах таблицы
                     if h in h_values:
                         # Высота находится в узле таблицы
-                        ro, T = self.__atmo_cache[h]
+                        ro, T, _, _, _ = self.__atmo_cache[h]
                         return ro, T
                     else:
                         # Высота не находится в узле таблицы
@@ -135,8 +190,8 @@ class F_prch_maker():
                                 h_r = h_s
                                 break
 
-                        ro_l, T_l = self.__atmo_cache[h_l]
-                        ro_r, T_r = self.__atmo_cache[h_r]
+                        ro_l, T_l, _, _, _ = self.__atmo_cache[h_l]
+                        ro_r, T_r, _, _, _ = self.__atmo_cache[h_r]
 
                         ro = self.__line_interpolation(h, h_l, h_r, ro_l, ro_r)
                         T = self.__line_interpolation(h, h_l, h_r, T_l, T_r)
@@ -155,18 +210,18 @@ class F_prch_maker():
                 if h <= h_min:
                     # Высота меньше минимальной из таблицы или совпадает с ней
                     h = h_min
-                    ro, T = (self.__atmo_cache['stand_atmo'])[h]
+                    ro, T, _, _, _ = (self.__atmo_cache['stand_atmo'])[h]
                     return ro, T
                 if h >= h_max:
                     # Высота превышает максимальную из таблицы или совпадает с ней
                     h = h_max
-                    ro, T = (self.__atmo_cache['stand_atmo'])[h]
+                    ro, T, _, _, _ = (self.__atmo_cache['stand_atmo'])[h]
                     return ro, T
                 else:
                     # Высота находится в пределах таблицы
                     if h in h_values:
                         # Высота находится в узле таблицы
-                        ro, T = (self.__atmo_cache['stand_atmo'])[h]
+                        ro, T, _, _, _ = (self.__atmo_cache['stand_atmo'])[h]
                         return ro, T
                     else:
                         # Высота не находится в узле таблицы
@@ -179,18 +234,74 @@ class F_prch_maker():
                                 h_r = h_s
                                 break
 
-                        ro_l, T_l = (self.__atmo_cache['stand_atmo'])[h_l]
-                        ro_r, T_r = (self.__atmo_cache['stand_atmo'])[h_r]
+                        ro_l, T_l, _, _, _ = (self.__atmo_cache['stand_atmo'])[h_l]
+                        ro_r, T_r, _, _, _ = (self.__atmo_cache['stand_atmo'])[h_r]
 
                         ro = self.__line_interpolation(h, h_l, h_r, ro_l, ro_r)
                         T = self.__line_interpolation(h, h_l, h_r, T_l, T_r)
 
                         return ro, T
 
-            def analysis_atmo(h):
+            def analysis_atmo(q):
+                h = q.height
                 ro_st, T_st = stand_atmo(h)
+                p_st = ro_st * self.__R_g * T_st
 
-            return stand_atmo
+                H = h / 1000
+                keys = ['tplot', 'tdavl', 'ttemp', 'tvetz', 'tvetm']
+
+                input_queues = []
+                output_queues = []
+                for key in keys:
+                    _, input_queue, output_queue = self.__process[key]
+                    input_queues.append(input_queue)
+                    output_queues.append(output_queue)
+                    input_queue.put((key, q.geodesy_rad[0], q.geodesy_rad[1], H))
+
+                for queues in input_queues:
+                    queues.join()
+
+                results = {}
+                for outs in output_queues:
+                    key, delta = outs.get()
+                    results[key] = delta
+
+                dro = results['tplot']
+                dp = results['tdavl']
+                dt = results['ttemp']
+                Wz = results['tvetz']
+                Wm = results['tvetm']
+
+                # results = {}
+                # threads = []
+                #
+                # def worker(key):
+                #     results[key] = delta_param(key, q, H)
+                #
+                # for key in keys:
+                #     thread = threading.Thread(target=worker, args=(key,))
+                #     threads.append(thread)
+                #     thread.start()
+                #
+                # for thread in threads:
+                #     thread.join()
+                #
+                # dro = results['tplot']
+                # dp = results['tdavl']
+                # dt = results['ttemp']
+                # Wz = results['tvetz']
+                # Wm = results['tvetm']
+
+                # dro = delta_param(keys[0], q, H)
+                # dp = delta_param(keys[1], q, H)
+                # dt = delta_param(keys[2], q, H)
+                # Wz = delta_param(keys[3], q, H)
+                # Wm = delta_param(keys[4], q, H)
+                print(f"{dro}, {dp}, {dt}, {Wz}, {Wm}")
+
+                return ro_st, T_st
+
+            return analysis_atmo
 
     def __analysis_Cx_cache(self):
         def analysis_Cx(v_norm, T):
@@ -246,7 +357,7 @@ class F_prch_maker():
                     # Переводим вектор на всякий случай в гринвичскую СК
                     q = q.in_gr
 
-                h = q.height
+
                 r, r_norm, r_ort = self.__r_comp(q)
                 v, v_norm, v_ort = self.__speed_comp(q)
                 W_c = (self.__omega_z ** 2) * r
@@ -256,7 +367,7 @@ class F_prch_maker():
                     f_accel += self.__grav_accel(r)
 
                 if self.__aero_d_resist is not None:
-                    ro, T = self.__analysis_atmo(h)
+                    ro, T = self.__analysis_atmo(q)
                     Cx = self.__analysis_Cx(v_norm, T)
                     f_accel += self.__aero_d_resist(ro, v_norm, v_ort, Cx, self.__s_mid_otd, self.__m_otd)
                 return np.concatenate((v, f_accel, [1]))
