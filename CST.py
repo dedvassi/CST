398767735e16b7d793a29426eb8497bdd13b5562
dedvassi <144363426+dedvassi@users.noreply.github.com>
2024-07-30 02:45:23 +0500

    ...

--- CST.py
+++ CST.py
@@ -1,12 +1,18 @@
+from math_logic.logic import main
+import multiprocessing
+import time
 import numpy as np
-
-class Q_v_state:
-
-    def __init__(self, test):
-        self.q = test
-        print(self.q)
+import sys
 
 
-q = Q_v_state('test')
-print(Q_v_state.__setattr__)
-#print(q.__dir__())
+if __name__ == '__main__':
+    # multiprocessing.set_start_method('spawn')
+    # print(np.pi)
+    if sys.platform.startswith('win'):
+        multiprocessing.set_start_method('spawn')
+        multiprocessing.freeze_support()
+    t1 = time.time()
+    main()
+    t2 = time.time()
+    print(t2 - t1)
+    input()
